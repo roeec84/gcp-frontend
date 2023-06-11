5f47655f66cd0a5cca6cec9d0c04447a4f9a357e
roeec84 <roeec84@gmail.com>
2023-06-11 16:37:20 +0300

    Very simple front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [tutorials, setTutorials] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('https://backend-app-shqy3cye6a-oa.a.run.app/api/tutorials')
+      setTutorials(res.data.map(x => x.title))
+    }
+    fetchData()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {tutorials.length > 0 && tutorials.map(tut => <div>{tut}</div>)}
+      </div>
+      <div>
+        If you can see the list above it is sign that we are connected to the backend and it takes data from SQL :)
+      </div>
     </div>
   );
-}
-
+};
 export default App;
